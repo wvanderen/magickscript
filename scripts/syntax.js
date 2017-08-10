11b8b273aab64540f2b9e8cf32515fd9f1367f13
William Van Deren <wvanderen@gmail.com>
2017-08-09 22:46:44 -0500

    initial commit

---
+++ scripts/syntax.js
@@ -0,0 +1,50 @@
+var magickScript = "MagickScript is a Magickal programming language based on JavaScript which utilizes JQuery functions." +
+"It is a hybrid digital and magickal system. Intentions, ingredients, energies, entities, can call be set as variables" +
+"which are called upon in functions, which are analogous to magickal procedures which are replicated in the code" +
+"for the function and which achieve a designated magickal result. The code replicates this by performing operations" +
+"on the supplied variables. The operation of the code acts as a magical act itself, giving the act power." +
+"This can be done in any number of ways. It may set a certain element, which may represent a specific area," +
+"to somethign which represents the magical intention. The only limits are the imagination and what you can do" +
+"with code. The code may just as a structure which represents the ritual and gives it direction, but the" +
+"combination of the computational arrangement of symbolical elements becomes much stronger when an actual magick" +
+"act relating to the content within is achieved by executing said code. MagickScript can act as an organizational" +
+"system for already existing sites. This document and accompanying ones contain the magickal DNA of MagickScript and makes it available to" +
+"files which pull them will have access to the library."
+
+//Basic syntactical structure see below for definitions in the context of MagickScript//
+var magickScriptSyntax = "$('element' || '#tag .class' || '.class:pseudoclass' || variable).event.method( {"
+  //function or procedure goes here
+  //If arguments can create branching procedures based on varying conditions
+  //For loops can affect change on many things in a row
+  //result return instructions go here
++ "})";
+
+//Definitions
+var variableDefinition = "Anything taken as an input or output in the magickal operation." +
+"Can be taken as an argument of a function or be set from its result or as part of its workings." +
+"Also can be a useful way of storing information and keeping it current.";
+
+var classDefinition = "A descriptor of variables. Can be used to group objects together to perform operations." +
+"Often relates to variables that accomplish a specific goal or relate to a specific function or that should be" +
+"styled together";
+
+var tagDefinition = "A grouping of tags by subject matter. Correspond to major long term projests and each may have" +
+"their own HTML file. Used to group classes in MagickScript interfaces.";
+
+var eventDefinition = "Something that acts as a trigger for the function/magickal operation. The language listens" +
+"for the event and triggers the function when it detects it."
+
+var methodDefinition = "Functions built into MagickScript, can be quite flexible. See methods.js for library."
+
+var functionDefinition = "Process by which an operation is completed. Result of function often relates to specific" +
+"output wanted or variables to be set. Can take arguments which are processed by the function."
+
+//Explanations of use of HTML and CSS files:
+var htmlEx = "HTML can be used to build actual rendering digital styles that are triggered" +
+"when the code is run. It can also be used in a more abstract way to structure ideas, plans and projects."
+
+var cssEx = "CSS is used to describe and style classes and elements. These may be tied to those desingated in HTML or" +
+"they can stand alone. Classes can be easily called in MagickScript and can be very useful for designating certain" +
+"points of focus or initiatives. Classes are grouped into tags which represent a smaller number of large" +
+"long-term areas of development and focus. Properties can be set on selected classes or elements, a list of properties" +
+"developed so far can be found in 'styles/methods.css'"
